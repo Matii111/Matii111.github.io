5714827ce19ce59315595042da873e0e98ab5f7c
matii111 <m.chaparrodaz@uandresbello.edu>
2023-04-28 01:17:21 -0400

    react currently working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './index.css';
-
+import working from './imgs/working.jpg';
+import git from './imgs/git.png';
 function App() {
   return (
     <div>
       <body>
         <div className="containerWorking">
           <div className="image-containerWorking">
-            <img className="pfp" src="./components/imgs/working.jpg" alt="Circular Image" />
+            <img className="pfp" src={working}/>
           </div>
           <p className="workingPhrase">404 - Work in progress</p>
           <a href="https://github.com/Matii111" rel="noreferrer">
-            <img className="git" src="./components/imgs/git.png" />
+            <img className="git" src={git} />
           </a>
         </div>
       </body>

1fdf5a3a61e8364fac9df6d936db117e28ac53e0
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-05-27 17:35:29 -0400

    style(major): New style for presentation label added.

---
+++ src/components/presentationLabel.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Code from '../imgs/logos/code-icon.svg';
+function PresentationLabel() {
+    return (
+        <div className='presentation-container'>
+            <div className='presentation-label'>
+                <p className='presentation'>Hola, soy &lt;Matias&gt;</p>
+                <p className='description'>Programador Junior</p>
+
+            </div>
+            <div className='presentation-logo'>
+                <img src={Code} alt='code' />
+            </div>
+        </div>
+    );
+};
+export default PresentationLabel;

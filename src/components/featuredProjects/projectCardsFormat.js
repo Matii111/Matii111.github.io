8d21de5dff90099fbf55b725d3c7e58a42506e8a
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-05-27 17:33:55 -0400

    style(minor): Featured content slide style improved.

--- src/components/featuredProjects/projectCardsFormat.js
+++ src/components/featuredProjects/projectCardsFormat.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Slide } from 'react-slideshow-image';
 import ProjectCardsData from './projectCardsData';
-
+import Next from '../../imgs/logos/next-slider-icon.svg';
+import Prev from '../../imgs/logos/prev-slider-icon.svg';
 function ProjectCard() {
     const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -15,38 +16,57 @@ function ProjectCard() {
         slidesToShow: 3,
         canSwipe: false,
         defaultIndex: 0,
-        prevArrow: <button className='slideButton' />,
-        nextArrow: <button className='slideButton' />,
+        transitionDuration: 500,
+        prevArrow: (
+            <button className='slideButtonPrev'>
+                <img src={Prev} alt="Prev" />
+            </button>
+        ),
+        nextArrow: (
+            <button className='slideButtonNext'>
+                <img src={Next} alt="Next" />
+            </button>
+        ),
         onStartChange: handleStartChange
     };
 
     return (
-        <div className="slide-container">
-            <Slide {...slideSettings}>
-                {ProjectCardsData.map((project, index) => {
-                    const nextSlide = (currentSlide + 1) % ProjectCardsData.length;
-                    return (
-                        <div
-                            className=
-                            {`project-card ${currentSlide === index ? '' :
-                                nextSlide === index ? 'middle-card' : ''
-                                }`}
-                            key={index}
-                        >
-                            <p className="project-title">{project.titulo}</p>
-                            <hr />
-                            <div className="project-info">
-                                <div className="project-img-container">
-                                    <img src={project.image} alt={project.titulo} />
-                                </div>
-                                <div className="project-description">
-                                    <p>{project.content}</p>
+        <div>
+            <div className='featured-title'>
+                <div className='circle-title' ></div>
+                <hr className='left-hr' />
+                <p>
+                    Proyectos destacados
+                </p>
+                <hr className='right-hr' />
+            </div>
+            <div className="slide-container">
+                <Slide {...slideSettings}>
+                    {ProjectCardsData.map((project, index) => {
+                        const nextSlide = (currentSlide + 1) % ProjectCardsData.length;
+                        return (
+                            <div
+                                className=
+                                {`project-card ${currentSlide === index ? '' :
+                                    nextSlide === index ? 'middle-card' : ''
+                                    }`}
+                                key={index}
+                            >
+                                <p className="project-title">{project.titulo}</p>
+                                <hr />
+                                <div className="project-info">
+                                    <div className="project-img-container">
+                                        <img src={project.image} alt={project.titulo} />
+                                    </div>
+                                    <div className="project-description">
+                                        <p>{project.content}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })}
-            </Slide>
+                        );
+                    })}
+                </Slide>
+            </div>
         </div>
     );
 }

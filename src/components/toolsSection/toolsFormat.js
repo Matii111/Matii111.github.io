7f5918a060d0959533e2bd557a07e6ab52b73c2a
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-05-27 17:35:55 -0400

    style(minor): Tools section style improved.

--- src/components/toolsSection/toolsFormat.js
+++ src/components/toolsSection/toolsFormat.js
@@ -1,13 +1,23 @@
 import ToolsIcons from "./toolsData";
 function ToolsIconsFormat() {
     return (
-        <div className='tools-content'>
-            {ToolsIcons.map((tools, index) => (
-                <div className='tool-item' key={index}>
-                    <img src={tools.image} alt={tools.titulo} />
-                </div>
-            ))}
-        </div>
+        <>
+            <div className='tools-title'>
+                <div className='circle-title' ></div>
+                <hr className='left-hr' />
+                <p>
+                    Especialidades
+                </p>
+                <hr className='right-hr' />
+            </div>
+            <div className='tools-content'>
+                {ToolsIcons.map((tools, index) => (
+                    <div className='tool-item' key={index}>
+                        <img src={tools.image} alt={tools.titulo} />
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
 

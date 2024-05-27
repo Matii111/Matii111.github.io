b54a3f2175a21e85346911a8f2290fee9cca5ed5
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-05-27 17:34:19 -0400

    style(minor): Footer style improved.

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,18 +4,24 @@ import linkedinLogo from '../imgs/logos/linkedin-icon.svg';
 function Footer() {
     return (
         <div className='footer-container'>
-            <h1>Muchas gracias por llegar hasta aqui</h1>
+            <div className='footer-title'>
+                <div className='circle-title' ></div>
+                <hr className='left-hr' />
+                <p>Muchas gracias por llegar hasta aqui</p>
+                <hr className='right-hr' />
+                <div className='circle-title' ></div>
+            </div>
             <div className='footer-items'>
                 <p>
                     Redes de contacto
                 </p>
                 <div className='items-icons'>
-                    <img src={gitHubLogo} alt='Redirigir a GitHub'/>                    
-                    <img src={mailLogo} alt='Copiar correo'/>
-                    <img src={linkedinLogo} alt='Redirigir a linkedin'/>
+                    <img src={gitHubLogo} alt='Redirigir a GitHub' />
+                    <img src={mailLogo} alt='Copiar correo' />
+                    <img src={linkedinLogo} alt='Redirigir a linkedin' />
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 

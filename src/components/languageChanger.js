f7da8f47c5f7beb17979ecce6ba20e6458221a00
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-05-27 17:34:44 -0400

    style(minor): Language bar style improved.

---
+++ src/components/languageChanger.js
@@ -0,0 +1,12 @@
+import React from "react";
+function LanguageChanger() {
+    return (
+        <>
+            <div className='language-changer'>
+                <p>&gt;&gt;current language</p>
+            </div>
+        </>
+    );
+};
+
+export default LanguageChanger;

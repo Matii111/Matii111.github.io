c88dc1c13070c8456e1d614806812e064ab176ec
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-05-27 17:31:01 -0400

    fix(minor): Main file correction.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import ProjectCard from './components/featuredProjects/projectCardsFormat';
 import CareerCard from './components/careerCards';
 import ToolsIcons from './components/toolsSection/toolsFormat';
 import Footer from './components/footer';
+import PresentationLabel from './components/presentationLabel';
+import LanguageChanger from './components/languageChanger';
 function App() {
   return (
     <div>
       <body>
-        <div className="containerWorking">
+        {/* <div className="containerWorking">
           <div className="image-containerWorking">
             <img className="pfp" src={working}/>
           </div>
@@ -17,27 +19,20 @@ function App() {
           <a href="https://github.com/Matii111" rel="noreferrer">
             <img className="git" src={git} />
           </a>
-        </div>
-        {/* <div>
-          <div className='language-changer'>
-            <h1>current language</h1>
-          </div>
-          <div className='presentation-label'>
-            <h1>Matias</h1>
-          </div>
-          <p className='section-title'>
-            Proyecto destacado
-          </p>
+        </div> */}
+        <div>
+          <LanguageChanger />
+          <PresentationLabel />
           <div className='content-container'>
             <ProjectCard />
             <div className='career-container'>
-              <h1 className='career-title'>
+              <p className='career-title'>
                 Trayectoria
-              </h1>
+              </p>
               <hr className='career-title-hr' />
-              <h2 className='career-subtitle'>
+              <p className='career-subtitle'>
                 Formacion
-              </h2>
+              </p>
               <hr className='career-subtitle-hr' />
               <div className='career-content-container'>
                 <h3 className='career-content-title'>Formacion 1</h3>
@@ -46,14 +41,11 @@ function App() {
               <hr className='divisor-bar'></hr>
             </div>
             <div className='tools-container'>
-              <h1 className='tools-title'>
-                Tecnologias utilizadas
-              </h1>
               <ToolsIcons />
             </div>
           </div>
           <Footer />
-        </div> */}
+        </div>
       </body>
     </div>
   );

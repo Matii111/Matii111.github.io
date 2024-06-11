21053fc2b7c44f379365d5f2bd06574e2165994e
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-06-11 02:20:06 -0400

    chore(minor): Changes in data format.

--- src/components/featuredProjects/projectCardsData.js
+++ src/components/featuredProjects/projectCardsData.js
@@ -1,19 +1,34 @@
 import project from '../../imgs/1280x720.svg';
 const ProjectCardsData = [
     {
-        titulo: 'Proyecto 1',
+        id: 1,
+        title: 'Proyecto 1',
         image: project,
+        image2: project,
+        image3: project,
+        image4: project,
         content: 'Descripción del proyecto 1.',
+        link: 'https://github.com/Matii111',
     },
     {
-        titulo: 'Proyecto 2',
+        id: 2,
+        title: 'Proyecto 2',
         image: project,
+        image2: project,
+        image3: project,
+        image4: project,
         content: 'Descripción del proyecto 2.',
+        link: 'https://github.com/Matii111',
     },
     {
-        titulo: 'Proyecto 3',
+        id: 3,
+        title: 'Proyecto 3',
         image: project,
+        image2: project,
+        image3: project,
+        image4: project,
         content: 'Descripción del proyecto 3.',
+        link: 'https://github.com/Matii111',
     },
 ];
 
--- src/components/featuredProjects/projectCardsFormat.js
+++ src/components/featuredProjects/projectCardsFormat.js
@@ -3,13 +3,28 @@ import { Slide } from 'react-slideshow-image';
 import ProjectCardsData from './projectCardsData';
 import Next from '../../imgs/logos/next-slider-icon.svg';
 import Prev from '../../imgs/logos/prev-slider-icon.svg';
+import Close from '../../imgs/logos/close-icon.svg';
+
 function ProjectCard() {
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [isProjectOpen, setProjectOpen] = useState(false);
+    const [selectedId, setSelectedId] = useState(null);
+
+    const handleProjectOpen = (id) => {
+        setSelectedId(id);
+        setProjectOpen(true);
+        document.body.style.overflow = 'hidden';
+    }
+
+    const handleProjectClose = () => {
+        setProjectOpen(false);
+        document.body.style.overflow = 'auto';
+    }
 
     const handleStartChange = (from, to) => {
-        console.log(`Starting transition from slide ${from} to slide ${to}`);
         setCurrentSlide(to);
     };
+    const project = ProjectCardsData.find(project => project.id === selectedId);
 
     const slideSettings = {
         autoplay: false,
@@ -41,22 +56,50 @@ function ProjectCard() {
                 <hr className='right-hr' />
             </div>
             <div className="slide-container">
+                {isProjectOpen && (
+                    <div className='overlay'>
+                        <div className='project-preview-container'>
+                            <div className='project-preview'>
+                                <div className='project-preview-left'>
+                                    <div className='project-preview-title'>
+                                        <div className='circle-title' ></div>
+                                        <hr className='left-hr' />
+                                        <p>
+                                            {project.title}
+                                        </p>
+                                    </div>
+                                    <p>
+                                        {project.content}
+                                    </p>
+                                    <p className='project-review-link'>
+                                        &gt;&gt;{project.link}
+                                    </p>
+                                </div>
+                                <hr className='right-hr' />
+                                <div className='project-preview-right'>
+                                    <img src={project.image} />
+                                    <img src={project.image2} />
+                                </div>
+                            </div>
+                            <p onClick={handleProjectClose} className='overlay-close-button'>
+                                <img src={Close} alt="Close" />
+                            </p>
+                        </div>
+                    </div>
+                )}
                 <Slide {...slideSettings}>
                     {ProjectCardsData.map((project, index) => {
                         const nextSlide = (currentSlide + 1) % ProjectCardsData.length;
                         return (
                             <div
-                                className=
-                                {`project-card ${currentSlide === index ? '' :
-                                    nextSlide === index ? 'middle-card' : ''
-                                    }`}
+                                className={`project-card ${currentSlide === index ? '' : nextSlide === index ? 'middle-card' : ''}`}
                                 key={index}
                             >
-                                <p className="project-title">{project.titulo}</p>
+                                <p className="project-title">{project.title}</p>
                                 <hr />
                                 <div className="project-info">
-                                    <div className="project-img-container">
-                                        <img src={project.image} alt={project.titulo} />
+                                    <div className="project-img-container" onClick={() => handleProjectOpen(project.id)}>
+                                        <img src={project.image} alt={project.title} />
                                     </div>
                                     <div className="project-description">
                                         <p>{project.content}</p>

7aba5ff10740f1e90c31c9dbcbff1d196af7d0be
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-06-14 18:43:21 -0400

    chore(minor): Test data modified.

---
+++ src/components/careerSection/careerData.js
@@ -0,0 +1,19 @@
+import project from '../../imgs/1280x720.svg';
+
+const CareerData = [
+    {
+        id: 1,
+        formation_title: 'Titulo de formacion (2000-2024)',
+        start_year: '2000',
+        end_year: '2024',
+        formation_content:
+            [
+                { year: '2001', description: 'Descripcion larga 1' },
+                { year: '2003', description: 'Descripcion' },
+                { year: '2004', description: 'Descripcion larga 3' },
+            ],
+        demo_image: { project }
+    },
+];
+
+export default CareerData;
--- src/components/featuredProjects/projectCardsData.js
+++ src/components/featuredProjects/projectCardsData.js
@@ -30,6 +30,26 @@ const ProjectCardsData = [
         content: 'Descripción del proyecto 3.',
         link: 'https://github.com/Matii111',
     },
+    {
+        id: 4,
+        title: 'Proyecto 4',
+        image: project,
+        image2: project,
+        image3: project,
+        image4: project,
+        content: 'Descripción del proyecto 3.',
+        link: 'https://github.com/Matii111',
+    },
+    {
+        id: 5,
+        title: 'Proyecto 5',
+        image: project,
+        image2: project,
+        image3: project,
+        image4: project,
+        content: 'Descripción del proyecto 3.',
+        link: 'https://github.com/Matii111',
+    }
 ];
 
 export default ProjectCardsData;

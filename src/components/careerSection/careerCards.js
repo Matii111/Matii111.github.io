7a4e9ccad1207ac0d092526f64f9c33ed30960e2
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-06-14 18:46:10 -0400

    style(major): Career section improved.

--- src/components/careerSection/careerCards.js
+++ src/components/careerSection/careerCards.js
@@ -1,31 +1,55 @@
+import CareerData from "./careerData";
+
+const TitleFixer = ({ featured_description }) => {
+    const featuredTitleLong = featured_description.length > 11;
+
+    return (
+        <p className={featuredTitleLong ? 'horizontal-hr-long' : ""}>
+            {featured_description}
+        </p>
+    );
+}
+
 function CareerCard() {
     return (
-        <div className='career-content'>
-            <p className='career-content-title'>
-                Titulo de formacion 2000-2000
-            </p>
-            <div className='career-line'>
-                <div className='career-start-line'>
-                    <div className='circle-title' />
-                    <p>2001</p>
-                </div>
-                <hr className='career-long-hr'></hr>
-                <div className='career-end-line'>
-                    <div className='circle-title' />
-                    <p>2024</p>
-                </div>
-                <div className='career-featured-content'>
-                    <hr className='diagonal-hr'></hr>
-                    <div className='circle-title' />
-                    <div className='horizontal-hr'>
-                        <hr />
-                        <p>dsssssssss</p>
+        <>
+            {CareerData.map((content, index) => {
+                return (
+                    <div className='career-content' key={index}>
+                        <p className='career-content-title'>
+                            {content.formation_title}
+                        </p>
+                        <div className='career-line'>
+                            <div className='career-start-line'>
+                                <div className='circle-title' />
+                                <p>{content.start_year}</p>
+                            </div>
+                            <hr className='career-long-hr'></hr>
+                            <div className='career-end-line'>
+                                <div className='circle-title' />
+                                <p>{content.end_year}</p>
+                            </div>
+                            <div className='career-featured-container'>
+                                {content.formation_content.map((featured, index) => (
+                                    <div className='career-featured-content'
+                                        style={index === 2 ? { marginLeft: '5rem' } : {}}
+                                        key={index}>
+                                        <hr className='diagonal-hr'></hr>
+                                        <div className='circle-title' />
+                                        <div className='horizontal-hr'>
+                                            <hr />
+                                            <TitleFixer featured_description={featured.description} />
+                                        </div>
+                                        <p>{featured.year}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <p className='career-content-description'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                     </div>
-                    <p>2024</p>
-                </div>                
-            </div>
-            <p className='career-content-description'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        </div>
+                );
+            })}
+        </>
     );
 }
 

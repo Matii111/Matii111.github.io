278e9270fce3fcf0ee938bad62e62b4001ae9e97
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-08-22 15:34:37 -0400

    fix(minor): Summary section responsive.

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,10 +1,12 @@
+import { Slide } from 'react-slideshow-image';
 function SummarySection() {
     return (
         <div className='summary-container'>
-
-            <p className='summary-title'>
-                Sobre mi &gt;&gt;
-            </p>
+            <div className='summary-title'>
+                <p>
+                    Sobre mi &gt;&gt;
+                </p>
+            </div>
             <div className='sumaries-container'>
                 <div className='summary-left'>
                     <p>
@@ -15,14 +17,12 @@ function SummarySection() {
                 <div className='summary-right'>
                     <p>
                         Conocimiento y experiencia intermedia con Stack Mern y Scrum++. Nivel de ingles B1.
-                    </p>
-                    <p>
                         Como metas personales, profundizar en JS y posteriormente ciberseguridad.
                     </p>
-                    <div>
-                        <a> Curriculum resumen &gt;&gt;</a>
-                    </div>
                 </div>
+            </div>
+            <div className='summary-curriculum'> 
+                <a> Curriculum resumen &gt;&gt;</a>
             </div>
         </div >
     );

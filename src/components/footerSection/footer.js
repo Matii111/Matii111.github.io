806301ad891648d30cb201790387e98369274a94
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-08-23 14:53:17 -0400

    feat(major): -> Footer data format added. -> Footer data added. -> Footer urls added.

---
+++ src/components/footerSection/footer.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { ReactComponent as GitHubLogo } from '../../imgs/logos/github-icon.svg';
+import { ReactComponent as MailLogo } from '../../imgs/logos/mail-icon.svg';
+import { ReactComponent as LinkedinLogo } from '../../imgs/logos/linkedin-icon.svg';
+
+import FooterData from './footerData';
+
+function Footer() {
+    const [hover, setHover] = useState(false);
+    const [mail, setMail] = useState(false);
+
+    return (
+        <div className='footer-container'>
+            <div className='footer-title'>
+                <div className='circle-title' ></div>
+                <hr className='left-hr' />
+                <p>Muchas gracias por llegar hasta aqui</p>
+                <hr className='right-hr' />
+                <div className='circle-title' ></div>
+            </div>
+            <div className='footer-items'>
+                <p>
+                    Redes de contacto
+                </p>
+                <div className='items-icons'>
+                    {FooterData.map((footer, index) => {
+                        return (
+                            <div className='items-icons'
+                                key={index}
+                            >
+                                {
+                                    footer.id === 'github' ? (
+                                        <a
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            href={footer.content}
+                                        >
+                                            <GitHubLogo className='footer-logos' alt='Redirigir a GitHub' />
+                                        </a>
+                                    ) :
+                                        footer.id === 'linkedin' ? (
+                                            <a
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                href={footer.content}
+                                            >
+                                                <LinkedinLogo className='footer-logos' alt='Redirigir a linkedin' />
+                                            </a>
+                                        ) :
+                                            footer.id === 'mail' ? (
+                                                <a
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className='footer-item-mail'
+                                                    onMouseEnter={() => setHover(true)}
+                                                    onMouseLeave={() => setHover(false)}
+                                                    onClick={() => {
+                                                        setMail(true);
+                                                        navigator.clipboard.writeText(footer.mail);
+                                                        setTimeout(() => setMail(false), 2000);
+                                                    }
+                                                    }
+                                                >
+                                                    <MailLogo className='footer-logos' alt='Copiar correo' />
+                                                    <p className={`copy-color-mail ${hover ? 'hover' : ''}`}>Copiar mail</p>
+                                                    <p className={`copied-color-mail ${mail ? 'active' : ''}`}>Mail copiado</p>
+                                                </a>
+                                            ) : null
+                                }
+                            </div>
+                        )
+                    }
+                    )
+                    }
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default Footer;
---
+++ src/components/footerSection/footerData.js
@@ -0,0 +1,16 @@
+const FooterData =
+    [
+        {
+            id: 'github',
+            content: 'https://github.com/Matii111',
+        }, {
+            id: 'mail',
+            mail: 'matias.chaparro.mail@gmail.com'
+        },
+        {
+            id: 'linkedin',
+            content: 'https://www.linkedin.com/in/matias-chaparro-diaz-a6749027a/'
+        }
+    ];
+
+export default FooterData;

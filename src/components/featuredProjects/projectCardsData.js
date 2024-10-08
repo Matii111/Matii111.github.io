d24cfecefbdb263454d81e3b49b319ba43c926c8
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-08 00:51:11 -0300

    feat(major): Language changer added to project cards data.

--- src/components/featuredProjects/projectCardsData.js
+++ src/components/featuredProjects/projectCardsData.js
@@ -2,7 +2,8 @@ const PlaceholderIMG = 'https://res.cloudinary.com/dq6ua6vmy/image/upload/v17244
 const ProjectCardsData = [
     {
         id: 1,
-        title: 'WebPage Servcon',
+        title: 'Pagina web Servcon',
+        titleEn: 'WebPage Servcon',
         image: 'https://res.cloudinary.com/dq6ua6vmy/image/upload/v1724443185/servcon-1_ndrkao.png',
         image2: 'https://res.cloudinary.com/dq6ua6vmy/image/upload/v1724443185/servcon-2_g0dn9m.png',
         image3: 'https://res.cloudinary.com/dq6ua6vmy/image/upload/v1724443185/servcon-3_ea4msg.png',
@@ -13,13 +14,20 @@ planificacion, disenio, y despliegue de la plataforma. \
 
 Desarrollado mediante stack MERN y desplegado en VPS. \
 
-Plataforma desplagada en enlace.
-        `,
+Plataforma desplagada en enlace.`,
+        contentEn:
+            `Development of website for SERVCON company,\
+planning, design, and deployment of the platform. \
+
+Developed using MERN stack and deployed on VPS. \
+
+Platform displayed in link.`,
         link: 'https://www.servcon.cl',
     },
     {
         id: 2,
-        title: 'Extension Color Tester',
+        title: 'Probador de colores',
+        titleEn: 'Extension Color Tester',
         image: 'https://github.com/user-attachments/assets/af1cc75b-bbe7-4b09-a3f2-bf6e4c96a893',
         image2: 'https://github.com/user-attachments/assets/7ff735ed-015a-4685-bb07-cbdea94eb1d6',
         content:
@@ -27,36 +35,53 @@ Plataforma desplagada en enlace.
 de desarrollar una plataforma web. Permite seleccionar colores \
 al activar hover sobre las clases de los elementos, ademas de \
 copiar el color HEX y buscar por clases especificas.',
+        contentEn:
+            'Extension that allows you to test colors instantly \
+to develop a web platform. Allows you to select colors \
+by activating hover over the element classes, in addition to \
+copy the HEX color and search by specific classes.',
         link: 'https://github.com/Matii111/extension_test',
     },
     {
         id: 3,
-        title: 'Proyecto 3',
+        title: 'Electron-App',
+        titleEn: 'Electron-App',
         image: PlaceholderIMG,
         image2: PlaceholderIMG,
         image3: PlaceholderIMG,
         image4: PlaceholderIMG,
-        content: 'Descripción del proyecto 3.',
+        content: 'Tetris online, similar a tetris 99.',
+        contentEn: 'Tetris online, similar a tetris 99.',
         link: 'https://github.com/Matii111',
     },
     {
         id: 4,
-        title: 'Proyecto 4',
+        title: 'NPM-Package',
+        titleEn: 'NPM-Package',
         image: PlaceholderIMG,
         image2: PlaceholderIMG,
         image3: PlaceholderIMG,
         image4: PlaceholderIMG,
-        content: 'Descripción del proyecto 3.',
+        content: 'Paquete npm que traduzca con API o lo que sea \
+pero con animacion como la de cyberpunk, publicarla en NPM coso.',
+        contentEn: 'Paquete npm que traduzca con API o lo que sea \
+pero con animacion como la de cyberpunk, publicarla en NPM coso.',
         link: 'https://github.com/Matii111',
     },
     {
         id: 5,
-        title: 'Proyecto 5',
+        title: 'URL-SHORTER',
+        titleEn: 'URL-SHORTER',
         image: PlaceholderIMG,
         image2: PlaceholderIMG,
         image3: PlaceholderIMG,
         image4: PlaceholderIMG,
-        content: 'Descripción del proyecto 3.',
+        content: 'Acortador de url con gestor de las mismas y \
+para dejar citas en apa para copiar y pegar, quizas misma funcion \
+para latex.         ',
+        contentEn: 'Acortador de url con gestor de las mismas y \
+para dejar citas en apa para copiar y pegar, quizas misma funcion \
+para latex.         ',
         link: 'https://github.com/Matii111',
     }
 ];

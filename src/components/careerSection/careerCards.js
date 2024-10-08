1e72e1165159740a009fb6fa3efcb93048260958
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-08 00:50:23 -0300

    feat(major): Language changer added to career cards.

--- src/components/careerSection/careerCards.js
+++ src/components/careerSection/careerCards.js
@@ -11,23 +11,36 @@ const TitleFixer = ({ featured_description }) => {
     );
 }
 
-function CareerCard() {
+function CareerCard({ isTraslatedLanguage }) {
     return (
         <>
             {CareerData.map((content, index) => {
                 return (
                     <div className='career-content' key={index}>
                         <div>
-                            <p className='career-content-title'>
-                                <Fade
-                                    direction='down'
-                                    className='fade-effect'
-                                    triggerOnce='true'
-                                    fraction='1'
-                                >
-                                    {content.formation_title}
-                                </Fade>
-                            </p>
+                            {isTraslatedLanguage ? (
+                                <p className='career-content-title'>
+                                    <Fade
+                                        direction='down'
+                                        className='fade-effect'
+                                        triggerOnce='true'
+                                        fraction='1'
+                                    >
+                                        {content.formation_title_en}
+                                    </Fade>
+                                </p>
+                            ) :
+                                <p className='career-content-title'>
+                                    <Fade
+                                        direction='down'
+                                        className='fade-effect'
+                                        triggerOnce='true'
+                                        fraction='1'
+                                    >
+                                        {content.formation_title}
+                                    </Fade>
+                                </p>
+                            }
                         </div>
                         <Fade
                             direction='down'
@@ -46,19 +59,35 @@ function CareerCard() {
                                     <p>{content.end_year}</p>
                                 </div>
                                 <div className='career-featured-container'>
-                                    {content.formation_content.map((featured, index) => (
-                                        <div className='career-featured-content'
-                                            style={index === 2 ? { marginLeft: '5rem' } : {}}
-                                            key={index}>
-                                            <hr className='diagonal-hr'></hr>
-                                            <div className='circle-title' />
-                                            <div className='horizontal-hr'>
-                                                <hr />
-                                                <TitleFixer featured_description={featured.description} />
+                                    {isTraslatedLanguage ? (
+                                        content.formation_content_en.map((featured, index) => (
+                                            <div className='career-featured-content'
+                                                style={index === 2 ? { marginLeft: '5rem' } : {}}
+                                                key={index}>
+                                                <hr className='diagonal-hr'></hr>
+                                                <div className='circle-title' />
+                                                <div className='horizontal-hr'>
+                                                    <hr />
+                                                    <TitleFixer featured_description={featured.description} />
+                                                </div>
+                                                <p>{featured.year}</p>
                                             </div>
-                                            <p>{featured.year}</p>
-                                        </div>
-                                    ))}
+                                        ))
+                                    ) :
+                                        content.formation_content.map((featured, index) => (
+                                            <div className='career-featured-content'
+                                                style={index === 2 ? { marginLeft: '5rem' } : {}}
+                                                key={index}>
+                                                <hr className='diagonal-hr'></hr>
+                                                <div className='circle-title' />
+                                                <div className='horizontal-hr'>
+                                                    <hr />
+                                                    <TitleFixer featured_description={featured.description} />
+                                                </div>
+                                                <p>{featured.year}</p>
+                                            </div>
+                                        ))
+                                    }
                                 </div>
                             </div>
                         </Fade>
@@ -70,25 +99,50 @@ function CareerCard() {
                                     triggerOnce='true'
                                     fraction='0'
                                 >
-                                    {content.formation_description.map((text, index) => (
-                                        <div className="formation_description" key={index}>
-                                            {
-                                                text.type === 'title' ? (
-                                                    <div className="formation_description-title">
-                                                        <div class="circle-title" />
-                                                        <p className="title">
-                                                            {text.content}
-                                                        </p>
-                                                    </div>
-                                                ) :
-                                                    text.type === 'paragraph' ? (
-                                                        <p className="paragraph">
-                                                            {text.content}
-                                                        </p>
-                                                    ) : null
-                                            }
-                                        </div>
-                                    ))}
+                                    {isTraslatedLanguage ? (
+                                        content.formation_description_en.map((text, index) => (
+                                            <div className="formation_description" key={index}>
+                                                {
+                                                    text.type === 'title' ? (
+                                                        <div className="formation_description-title">
+                                                            <div class="circle-title" />
+                                                            <p className="title">
+                                                                {text.content}
+                                                            </p>
+                                                        </div>
+                                                    ) :
+                                                        text.type === 'paragraph' ? (
+                                                            <p className="paragraph">
+                                                                {text.content}
+                                                            </p>
+                                                        ) : null
+                                                }
+                                            </div>
+                                        ))
+
+
+                                    ) :
+                                        content.formation_description.map((text, index) => (
+                                            <div className="formation_description" key={index}>
+                                                {
+                                                    text.type === 'title' ? (
+                                                        <div className="formation_description-title">
+                                                            <div class="circle-title" />
+                                                            <p className="title">
+                                                                {text.content}
+                                                            </p>
+                                                        </div>
+                                                    ) :
+                                                        text.type === 'paragraph' ? (
+                                                            <p className="paragraph">
+                                                                {text.content}
+                                                            </p>
+                                                        ) : null
+                                                }
+                                            </div>
+                                        ))
+                                    }
+
                                 </Fade>
                             </p>
                         </div>

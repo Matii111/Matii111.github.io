656610b5a705c20214287d27a842f9c2bc3fad7c
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-08 00:52:18 -0300

    feat(major): Language changer added to summary data.

--- src/components/summarySection/summaryData.js
+++ src/components/summarySection/summaryData.js
@@ -3,10 +3,12 @@ const SummaryData =
         {
             id: 'leftContent',
             content: 'Conocimiento centrado en desarrollo de aplicaciones, software y plataformas web. Experiencia en proyectos bajo tutoria en academia, ademas de proyectos con clientes reales.',
+            contentEn: 'Knowledge focused on development of applications, software and web platforms. Experience in projects under tutoring in academia, as well as projects with real clients.'
         },
         {
             id: 'rightContent',
-            content: 'Conocimiento y experiencia intermedia con Stack Mern y Scrum++. Nivel de ingles B1. Como metas personales, profundizar en JS y posteriormente ciberseguridad.'
+            content: 'Conocimiento y experiencia intermedia con Stack Mern y Scrum++. Nivel de ingles B1. Como metas personales, profundizar en JS y posteriormente ciberseguridad.',
+            contentEn: 'Knowledge and intermediate experience with Stack Mern and Scrum++. English level B1. As personal goals, delve into JS and later cybersecurity.'
         },
         {
             id: 'URL',

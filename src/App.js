3d090e0cc7105389e637e82fb2ae1f574dbb7fb8
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-08 00:49:49 -0300

    feat(major): Language changer now works.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './index.css';
 import { Fade } from "react-awesome-reveal";
 import React from 'react';
+import { useState } from 'react';
 import ToolsIcons from './components/toolsSection/toolsFormat';
 import Footer from './components/footerSection/footer';
 import PresentationLabel from './components/presentationLabel';
@@ -16,7 +17,9 @@ import { ReactComponent as GitHubLogo } from '../src/imgs/logos/github-icon.svg'
 
 const pfp = "https://private-user-images.githubusercontent.com/101642846/359743782-ffa84a53-bde0-41a6-ba91-9a314875567a.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjQyMTM4NTIsIm5iZiI6MTcyNDIxMzU1MiwicGF0aCI6Ii8xMDE2NDI4NDYvMzU5NzQzNzgyLWZmYTg0YTUzLWJkZTAtNDFhNi1iYTkxLTlhMzE0ODc1NTY3YS5qcGc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwODIxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDgyMVQwNDEyMzJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zYjljNzEwNGYxYTZiMmMwM2UxYjE0NGZlYTNkOGQyN2E2ODM5MWM4M2ZhYmNlODhjNjExZWZlNzAyYzlkNzVjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.Txvdfom_F7VhSEJB9E1fro6HMIjgGVHGIj5jJgf_HVQ";
 
+
 function App() {
+  const [isTraslatedLanguage, setTraslatedLanguage] = useState(false);
   return (
     <div>
       <body>
@@ -42,14 +45,19 @@ function App() {
         */}
 
         <div>
-          <LanguageChanger />
+          <LanguageChanger
+            isTraslatedLanguage={isTraslatedLanguage}
+            setTraslatedLanguage={setTraslatedLanguage}
+          />
           <Fade
             direction='down'
             className='fade-effect'
             triggerOnce='true'
             fraction='0'
           >
-            <PresentationLabel />
+            <PresentationLabel
+              isTraslatedLanguage={isTraslatedLanguage}
+            />
           </Fade>
           <GoTop />
           <div className='content-container'>
@@ -59,14 +67,22 @@ function App() {
               triggerOnce='true'
               fraction='0'
             >
-              <SummarySection />
+              <SummarySection
+                isTraslatedLanguage={isTraslatedLanguage}
+              />
             </Fade>
-            <ProjectFormat />
+            <ProjectFormat
+              isTraslatedLanguage={isTraslatedLanguage}
+            />
             <div className='career-container'>
-              <CareerFormat />
+              <CareerFormat
+                isTraslatedLanguage={isTraslatedLanguage}
+              />
             </div>
             <div className='tools-container'>
-              <ToolsIcons />
+              <ToolsIcons
+                isTraslatedLanguage={isTraslatedLanguage}
+              />
             </div>
           </div>
           <Fade
@@ -75,7 +91,9 @@ function App() {
             triggerOnce='true'
             fraction='0.5'
           >
-            <Footer />
+            <Footer
+              isTraslatedLanguage={isTraslatedLanguage}
+            />
           </Fade>
         </div>
       </body >
--- src/components/languageChanger.js
+++ src/components/languageChanger.js
@@ -1,11 +1,21 @@
-function LanguageChanger() {
+import { useState } from 'react';
+
+function LanguageChanger({ isTraslatedLanguage, setTraslatedLanguage }) {
+    const languages = ['Español', 'English'];
+
+    const handleLanguageChange = () => {
+        setTraslatedLanguage(prevState => !prevState);
+    };
+
     return (
         <>
             <div className='language-changer'>
-                <p>&gt;&gt;current language</p>
+                <p onClick={handleLanguageChange}>
+                    &gt;&gt; {isTraslatedLanguage ? languages[1] : languages[0]}
+                </p>
             </div>
         </>
     );
-};
+}
 
 export default LanguageChanger;

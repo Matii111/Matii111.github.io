6e8e1c8a79e4e9eb752613a708fa130f641e6590
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-15 20:37:21 -0300

    feat(major): Traslator funcion workin.

---
+++ src/utils/traslator.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+export function DelayedText({
+    text,
+    delay = 30
+}) {
+    const [displayedText, setDisplayedText] = useState('');
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        if (text !== displayedText) {
+            setIndex(0);
+        }
+    }, [text]);
+
+    useEffect(() => {
+        if (index < text.length || displayedText.length > text.length) {
+            const timeout = setTimeout(() => {
+                const updatedText =
+                    text.substring(0, index + 1) +
+                    displayedText.substring(index + 1);
+                setDisplayedText(updatedText);
+                setIndex(prev => prev + 1);
+            }, delay);
+            return () => clearTimeout(timeout);
+        }
+    }, [index, text, displayedText]);
+
+    return <span>{displayedText}</span>;
+}

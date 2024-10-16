da22b10ff1c93d3a9bf6cc606f6c0c6f348ee464
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-16 00:58:07 -0300

    feat(major): Translate button now have delay.

--- src/components/languageChanger.js
+++ src/components/languageChanger.js
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function LanguageChanger({ isTraslatedLanguage, setTraslatedLanguage }) {
     const languages = ['Español', 'English'];
+    const [isTranslating, setTranslating] = useState(false);
 
     const handleLanguageChange = () => {
-        setTraslatedLanguage(prevState => !prevState);
+
+        if (!isTranslating) {
+            setTranslating(true);
+            setTraslatedLanguage(prevState => !prevState);
+
+            setTimeout(() => {
+                setTranslating(false);
+            }, 5000);
+        }
     };
 
     return (
         <>
             <div className='language-changer'>
-                <p onClick={handleLanguageChange}>
-                    &gt;&gt; {isTraslatedLanguage ? languages[1] : languages[0]}
+                <p
+                    onClick={handleLanguageChange}
+                    className={`${isTranslating ? 'language-changer-translating' : ''}`}
+                >
+                    &gt;&gt; {isTraslatedLanguage ? languages[0] : languages[1]}
                 </p>
             </div>
         </>
